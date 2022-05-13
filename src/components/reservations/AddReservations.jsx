153031c089d3772fdc8a9436efa798f59fdf554e
Busiwa24 <busiwaliuma@gmail.com>
2022-05-13 11:51:58 +0200

    Create form with initial inputs

--- src/components/reservations/AddReservations.jsx
+++ src/components/reservations/AddReservations.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
 import NavBar from '../navbar/NavBar';
 
+ 
+    <div className="w-full flex justify-center bg-gray-100 py-16">
+      <div className="w-1/2 shadow-md border bg-white pb-12">
+        <div className="py-3 px-8 border-b-2">
+          <h1>Add Movie</h1>
+        </div>
