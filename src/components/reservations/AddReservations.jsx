a732d6f5222dbf53bdf5c80064a6008c6cce09ee
Busiwa24 <busiwaliuma@gmail.com>
2022-05-13 13:27:12 +0200

    Enter additional reserve input

--- src/components/reservations/AddReservations.jsx
+++ src/components/reservations/AddReservations.jsx
@@ -36,3 +36,51 @@ import NavBar from '../navbar/NavBar';
                 id="releaseDate"
                 className="py-2 px-3 border border-gray-300 w-full"
               />
+              </label>
+            <label htmlFor="playingTime" className="block w-1/2">
+              <span className="block mb-1">Playing Time</span>
+              <input
+                type="time"
+                id="playingTime"
+                className="py-2 px-3 border border-gray-300 w-full"
+              />
+            </label>
+          </div>
+          <div className="flex mb-4">
+            <label htmlFor="genre" className="block mr-5 w-1/2">
+              <span className="block mb-1">Genre</span>
+              <input
+                type="text"
+                id="genre"
+                className="py-2 px-3 border border-gray-300 w-full"
+                placeholder="Enter movie genre"
+              />
+            </label>
+            <label htmlFor="ticketPrice" className="block w-1/2">
+              <span className="block mb-1">Ticket Price</span>
+              <input
+                type="text"
+                id="ticketPrice"
+                className="py-2 px-3 border border-gray-300 w-full"
+                placeholder="$ 0.0"
+              />
+            </label>
+          </div>
+          <div className="flex mb-4">
+            <label htmlFor="leadCast" className="block mr-5 w-1/2">
+              <span className="block mb-1">Lead Cast</span>
+              <input
+                type="text"
+                id="leadCast"
+                className="py-2 px-3 border border-gray-300 w-full"
+                placeholder="Enter lead cast name"
+              />
+            </label>
+            <label htmlFor="country" className="block w-1/2">
+              <span className="block mb-1">Country</span>
+              <input
+                type="text"
+                id="country"
+                className="py-2 px-3 border border-gray-300 w-full"
+                placeholder="Enter country"
+              />
